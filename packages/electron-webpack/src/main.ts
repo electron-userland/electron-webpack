e7d5c787a31a74dc372d0303438b1cf610b7eaf6
develar <develar@gmail.com>
2017-10-27 09:51:09 +0200

    feat: Support for tsx as entry file
    Close #42

--- packages/electron-webpack/src/main.ts
+++ packages/electron-webpack/src/main.ts
@@ -332,7 +332,7 @@ export async function configure(type: ConfigurationType, env: ConfigurationEnv |
 
 async function computeEntryFile(srcDir: string, projectDir: string): Promise<string | null> {
   const candidates: Array<string> = []
-  for (const ext of ["ts", "js"]) {
+  for (const ext of ["ts", "js", "tsx"]) {
     for (const name of ["index", "main", "app"]) {
       candidates.push(`${name}.${ext}`)
     }
