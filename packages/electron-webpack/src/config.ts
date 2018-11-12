electron-userland/electron-webpack

commit c4cf41158b8b3a763d4254980f417d74531c7ade

feat: Added electron builder config function (#221)
* Added electron builder config function

* Moved config to seperate file

* Moved electron builder config into seperate ts file

* Removed unneeded source map support

packages/electron-webpack/src/config.ts
@@ -1,0 +1,24 @@
+import { readJson } from "fs-extra-p"
+import { Lazy } from "lazy-val"
+import * as path from "path"
+import { orNullIfFileNotExist } from "./util"
+import { getConfig } from "read-config-file"
+
+export { ElectronWebpackConfiguration } from "./core"
+
+export async function getPackageMetadata() {
+  const projectDir = process.cwd()
+  return await orNullIfFileNotExist(readJson(path.join(projectDir, "package.json")))
+}
+
+export async function getElectronWebpackConfig() {
+  const projectDir = process.cwd()
+  const packageMetadata = await getPackageMetadata()
+  const electronWebpackConfig = ((await getConfig({
+    packageKey: "electronWebpack",
+    configFilename: "electron-webpack",
+    projectDir,
+    packageMetadata: new Lazy(() => Promise.resolve(packageMetadata))
+  })) || {} as any).result || {}
+  return electronWebpackConfig
+}

packages/electron-webpack/src/main.ts
@@ -2,7 +2,7 @@ import BluebirdPromise from "bluebird-lst"
 import { readJson } from "fs-extra-p"
 import { Lazy } from "lazy-val"
 import * as path from "path"
-import { getConfig, validateConfig } from "read-config-file"
+import { validateConfig } from "read-config-file"
 import { deepAssign } from "read-config-file/out/deepAssign"
 import "source-map-support/register"
 import { Configuration, Plugin, RuleSetRule } from "webpack"
@@ -13,7 +13,8 @@ import { ConfigurationEnv, ConfigurationType, ElectronWebpackConfiguration, Pack
 import { BaseTarget } from "./targets/BaseTarget"
 import { MainTarget } from "./targets/MainTarget"
 import { BaseRendererTarget, RendererTarget } from "./targets/RendererTarget"
-import { getFirstExistingFile, orNullIfFileNotExist } from "./util"
+import { getFirstExistingFile } from "./util"
+import { getElectronWebpackConfig, getPackageMetadata } from "./config"
 
 export { ElectronWebpackConfiguration } from "./core"
 
@@ -59,6 +60,7 @@ export class WebpackConfigurator {
 
   readonly sourceDir: string
   readonly commonSourceDirectory: string
+  readonly commonDistDirectory: string
 
   readonly debug = _debug(`electron-webpack:${this.type}`)
 
@@ -109,6 +111,7 @@ export class WebpackConfigurator {
 
     const commonSourceDirectory = this.electronWebpackConfiguration.commonSourceDirectory
     this.commonSourceDirectory = commonSourceDirectory == null ? path.join(this.projectDir, "src", "common") : path.resolve(this.projectDir, commonSourceDirectory)
+    this.commonDistDirectory = path.resolve(this.projectDir, this.electronWebpackConfiguration.commonDistDirectory || "dist")
   }
 
   /**
@@ -139,10 +142,6 @@ export class WebpackConfigurator {
     }
   }
 
-  get commonDistDirectory() {
-    return path.join(this.projectDir, "dist")
-  }
-
   hasDependency(name: string) {
     return name in this.metadata.dependencies || this.hasDevDependency(name)
   }
@@ -155,7 +154,7 @@ export class WebpackConfigurator {
    * Returns the names of devDependencies that match a given string or regex.
    * If no matching dependencies are found, an empty array is returned.
    *
-   * @return list of matching dependency names, e.g. `['@babel/preset-react', '@babel/preset-stage-0']`
+   * @return list of matching dependency names, e.g. `["@babel/preset-react", "@babel/preset-stage-0"]`
    */
   getMatchingDevDependencies(options: GetMatchingDevDependenciesOptions = {}) {
     const includes = options.includes || []
@@ -306,14 +305,7 @@ export async function createConfigurator(type: ConfigurationType, env: Configura
      env = {}
    }
 
-  const projectDir = (env.configuration || {}).projectDir || process.cwd()
-  const packageMetadata = await orNullIfFileNotExist(readJson(path.join(projectDir, "package.json")))
-  const electronWebpackConfig = ((await getConfig({
-    packageKey: "electronWebpack",
-    configFilename: "electron-webpack",
-    projectDir,
-    packageMetadata: new Lazy(() => Promise.resolve(packageMetadata))
-  })) || {} as any).result || {}
+  const electronWebpackConfig = await getElectronWebpackConfig()
   if (env.configuration != null) {
     deepAssign(electronWebpackConfig, env.configuration)
   }
@@ -328,6 +320,7 @@ How to fix:
   * Found? Check that the option in the appropriate place. e.g. "sourceDirectory" only in the "main" or "renderer", not in the root.
 `
   })
+  const packageMetadata = await getPackageMetadata()
   return new WebpackConfigurator(type, env, electronWebpackConfig, packageMetadata)
 }
 

packages/electron-webpack/electron-builder.yml
@@ -1,17 +1,0 @@
-extraMetadata:
-  main: main.js
-files:
-  -
-    from: .
-    # package.json is added to filter for main file set in any case, but some inclusive pattern must be in the filter, otherwise electron-builder will add */**
-    filter: ["package.json"]
-  -
-    from: "dist/main"
-  -
-    from: "dist/renderer"
-  -
-    from: "dist/renderer-dll"
-extraResources:
-  -
-    from: static
-    to: static

packages/electron-webpack/src/core.ts
@@ -16,6 +16,7 @@ export interface ElectronWebpackConfiguration {
   main?: ElectronWebpackConfigurationMain | null
 
   commonSourceDirectory?: string | null
+  commonDistDirectory?: string | null
 
   title?: string | boolean | null
 

packages/electron-webpack/src/dev/dev-runner.ts
@@ -10,6 +10,7 @@ import { configure } from "../main"
 import { getFreePort, orNullIfFileNotExist } from "../util"
 import { DelayedFunction, getCommonEnv, logError, logProcess, logProcessErrorOutput } from "./devUtil"
 import { startRenderer } from "./WebpackDevServerManager"
+import { getElectronWebpackConfig } from "../config"
 
 const projectDir = process.cwd()
 
@@ -19,7 +20,8 @@ const debug = require("debug")("electron-webpack")
 
 // do not remove main.js to allow IDE to keep breakpoints
 async function emptyMainOutput() {
-  const outDir = path.join(projectDir, "dist", "main")
+  const electronWebpackConfig = await getElectronWebpackConfig()
+  const outDir = path.join(projectDir, electronWebpackConfig.commonDistDirectory, "main")
   const files = await orNullIfFileNotExist(readdir(outDir))
   if (files == null) {
     return

packages/electron-webpack/src/electron-builder.ts
@@ -1,0 +1,31 @@
+import { getElectronWebpackConfig } from "./config"
+
+export default async function() {
+  const electronWebpackConfig = await getElectronWebpackConfig()
+  return {
+    extraMetadata: {
+      main: "main.js"
+    },
+    files: [
+      {
+        from: ".",
+        filter: ["package.json"]
+      },
+      {
+        from: electronWebpackConfig.commonDistDirectory + "/main"
+      },
+      {
+        from: electronWebpackConfig.commonDistDirectory + "/renderer"
+      },
+      {
+        from: electronWebpackConfig.commonDistDirectory + "/renderer-dll"
+      }
+    ],
+    extraResources: [
+      {
+        from: "static",
+        to: "static"
+      }
+    ]
+  }
+}

commit d4bd218072c5abbb65fdd36a0fd86d53cc3b1f07

Fix tests that broke with commit c4cf41158b8b3a763d4254980f417d74531c7ade

packages/electron-webpack/src/config.ts
@@ -6,19 +6,16 @@ import { getConfig } from "read-config-file"
 
 export { ElectronWebpackConfiguration } from "./core"
 
-export async function getPackageMetadata() {
-  const projectDir = process.cwd()
-  return await orNullIfFileNotExist(readJson(path.join(projectDir, "package.json")))
+export async function getPackageMetadata(projectDir?: string | null) {
+  return await orNullIfFileNotExist(readJson(path.join(projectDir || process.cwd(), "package.json")))
 }
 
-export async function getElectronWebpackConfig() {
-  const projectDir = process.cwd()
-  const packageMetadata = await getPackageMetadata()
-  const electronWebpackConfig = ((await getConfig({
+export async function getElectronWebpackConfig(projectDir?: string | null) {
+  const packageMetadata = await getPackageMetadata(projectDir)
+  return ((await getConfig({
     packageKey: "electronWebpack",
     configFilename: "electron-webpack",
-    projectDir,
+    projectDir: projectDir || process.cwd(),
     packageMetadata: new Lazy(() => Promise.resolve(packageMetadata))
   })) || {} as any).result || {}
-  return electronWebpackConfig
 }

packages/electron-webpack/src/main.ts
@@ -304,8 +304,8 @@ export async function createConfigurator(type: ConfigurationType, env: Configura
   if (env == null) {
      env = {}
    }
-
-  const electronWebpackConfig = await getElectronWebpackConfig()
+  const projectDir = (env.configuration || {}).projectDir
+  const electronWebpackConfig = await getElectronWebpackConfig(projectDir)
   if (env.configuration != null) {
     deepAssign(electronWebpackConfig, env.configuration)
   }
@@ -320,7 +320,7 @@ How to fix:
   * Found? Check that the option in the appropriate place. e.g. "sourceDirectory" only in the "main" or "renderer", not in the root.
 `
   })
-  const packageMetadata = await getPackageMetadata()
+  const packageMetadata = await getPackageMetadata(projectDir)
   return new WebpackConfigurator(type, env, electronWebpackConfig, packageMetadata)
 }
 